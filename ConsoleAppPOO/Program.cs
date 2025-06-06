e29c6909ee6274113ba2e0a2a5cd60d5a40c396c
Rian Felipe da Silva Silvestre <rian.felipe.1650@gmail.com>
2025-06-06 20:01:55 -0300

    Add files via upload

---
+++ ConsoleAppPOO/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleAppPOO.Controller;
+using ConsoleAppPOO.Model;
+
+/*
+Produto produto = new Produto();
+List<Produto> listaProduto = new List<Produto>();
+
+
+produto.Id = 1;
+produto.Descricao = "Bola";
+
+Console.WriteLine(produto.Descricao);
+// apresentar apaenas um produto
+//produto.Apresentar(produto);
+
+listaProduto.Add(produto);
+
+produto = new Produto();
+produto.Id = 2;
+produto.Descricao = "chuteira";
+
+
+listaProduto.Add(produto);
+// contrutor com parametros
+Produto produto2 = new Produto( 3, "luva");
+listaProduto.Add(produto2);
+
+
+produto.ApresentarLista(listaProduto);
+*/
+
+
+Bolacontroller controller = new Bolacontroller();
+List<Bola> lista = new List<Bola>();
+
+Bola bola = new Bola(1, "Topper" , 150m);
+lista = controller.Adicionar(bola);
+
+bola = new Bola(2, "Adidas", 300m);
+lista = controller.Adicionar(bola);
+
+controller.Lista(lista);
+
+
+
+
